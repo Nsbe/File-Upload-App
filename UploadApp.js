169eefa199bbb25b1e068b12c1f8e2f5de539042
lteagues27 <lteagues@chicagonsbe.org>
2016-12-28 09:16:10 -0600

    Create UploadApp.js

---
+++ UploadApp.js
@@ -0,0 +1,47 @@
+Var fs  = require(“fs”);
+Var files = fs.readdirSync(‘./lib’);
+Console.log(files);
+Var fs = require(“fs”);
+Fs.mkdir(“File Upload”, function(err) {
+If (err) {
+Consoles.log(err);
+} else {
+Console.log(“Loaded Documents”);
+}
+});
+
+<input id="upload=input" type="file" name="uploads[]" multiple="multiple">
+#upload-input {
+display: none;
+}
+.btn:focus, .upload-btn:focus{
+outline: 0;
+}
+
+html,
+body {
+height: 100%;
+}
+
+body {
+text-align: center;
+}
+
+.row {
+ margin-top: 60px;
+ }
+ 
+.upload-btn {
+  border: none;
+  }
+  
+  .upload-btn:hover,
+  .upload-btn:focus,
+  .upload-btn:active,
+  .upload-btn.active {
+  border: none;
+  }
+  
+  
+  
+  
